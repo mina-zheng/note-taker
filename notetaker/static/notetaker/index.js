96a8c32ad3bd4aea5eb9e03a31f772e9fe3d23ac
MINA <minazheng505@gmail.com>
2025-08-25 12:41:19 -0400

    added js

--- notetaker/static/notetaker/index.js
+++ notetaker/static/notetaker/index.js
@@ -1,2 +1,36 @@
-const box = document.querySelector(".box");
-box.style.display = "none";
+function toggleAddBox(button) {
+    const form = button.nextElementSibling;
+    if (form) {
+        if (form.style.display == 'none') {
+            form.style.display = 'block';
+        }
+        else {
+            form.style.display = 'none';
+        }
+    }
+}
+    
+
+function toggleEditBox(button) {
+    const form = button.nextElementSibling;
+    if (form) {
+        if (form.style.display == 'none') {
+            form.style.display = 'block';
+        }
+        else {
+            form.style.display = 'none';
+        }
+    }
+}
+
+function toggleEditNote(button) {
+    const form = button.previousElementSibling;
+    if (form) {
+        if (form.style.display == 'none') {
+                form.style.display = 'block';
+            }
+        else {
+            form.style.display = 'none';
+        }
+    }
+}
